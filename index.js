c9ffcc924c2eee41fdc2447b9a58bf34864dc873
Paige <prhoward97@gmail.com>
2022-10-01 20:15:05 -0400

    commit tree 57936d53e0c8e4fbf38d0fef534b5f036d9eb802

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const arrayCards = [];
+    for (let i = 0; i < names.length; i++) {
+        arrayCards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`); 
+       
+        //*debugger;
+    }console.log(arrayCards);
+    return arrayCards;
+
+
+}
+
+
+
+
+function countDown(startingNumber) {
+    
+    //let  = 0;
+    while (startingNumber > 0) {
+        
+        console.log(startingNumber);
+        startingNumber -= 1;
+    };
+    
+    console.log(startingNumber);
+}
+
+
+
+
